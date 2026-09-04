ae7881b80d52cd58145e878cb7dca963101107fd
Chin Chanvattey <belljoliebell002@gmail.com>
2026-09-04 14:50:48 +0700

    Initial small shop project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Product.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "====================================" << endl;
+    cout << "       SMALL SHOP INVENTORY" << endl;
+    cout << "====================================" << endl;
+
+    cout << "Program started successfully!" << endl;
+
+    return 0;
+}
